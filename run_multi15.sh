bc559b46f8e16003f5fb21112bbea916fc49deff
e16005 <fukuda@brain.imi.i.u-tokyo.ac.jp>
2015-03-28 10:50:41 +0900

    学習が進まない理由を検証中

---
+++ run_multi15.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -x
+#
+#PJM --rsc-list "rscgrp=short"
+#PJM --rsc-list "node=62"
+#PJM --rsc-list "elapse=01:10:00"
+#PJM --mpi "proc=992"
+#PJM --stg-transfiles all
+#PJM --mpi "use-rankdir"
+
+#PJM --stgin "rank=* ./* %r:./"
+#PJM --stgin "rank=* ./data/* %r:./data/"
+#PJM --stgin "rank=* ./weights/* %r:./weights/"
+#PJM --stgin "rank=* ./result/* %r:./result/"
+#PJM --stgin "rank=* ./change/* %r:./change/"
+
+#PJM --stgin "rank=* /home/e16005/neuron_kplus/specials/sparc64/special %r:./"
+
+#PJM --stgout "rank=* %r:./result/* /group/gh16/e16005/0108/result/sw/%j/"
+#PJM --stgout "rank=* %r:./change/* /group/gh16/e16005/0108/change/sw/%j/"
+
+#PJM -s
+#PJM -m "e"
+#
+
+
+#NRNIV="../../../neuron_kplus/specials/sparc64/special -mpi"
+NRNIV="./special -mpi"
+HOC_NAME="./network.hoc"
+
+NRNOPT=\
+" -c OUT1_E=0.01"\
+" -c OUT1_I=0.01"\
+" -c IO_E=0.7"\
+" -c IO_I2E=0.003"\
+" -c IO_I2I=0.001"\
+" -c OUT1_SPON_E_K=0.25"\
+" -c OUT1_SPON_E_T=0.35"\
+" -c DOPAMINE=0.01"\
+" -c LEARNING_RATE=0.025"\
+" -c OUT1_SPON_I_K=0.15"\
+" -c OUT1_SPON_I_T=0.3"\
+" -c LTD=0.5"
+
+
+LPG="lpgparm -t 4MB -s 4MB -d 4MB -p 4MB"
+MPIEXEC="mpiexec -mca mpi_print_stats 1"
+PROF=""
+echo "${PROF} ${MPIEXEC} ${LPG} ${NRNIV} ${HOC_NAME}"
+time ${PROG} ${MPIEXEC} ${LPG} ${NRNIV} ${NRNOPT} ${HOC_NAME}
+
+sync
